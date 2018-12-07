9c14bcb12d98248394e88b0de499ecdb28d98960
Michael High <mhigh85@gmail.com>
2018-12-07 17:38:00 -0500

    Refactored to minimize duplicate code blocks

--- liri.js
+++ liri.js
@@ -1,21 +1,27 @@
+// Load node.js modules
 require('dotenv').config();
 const fs = require('fs');
 const request = require('request');
 const Spotify = require('node-spotify-api');
+
+// Load api keys
 const keys = require('./keys');
 const spotify = new Spotify(keys.spotify);
 
-// concert-this
+// Query the BandsInTown API for an Artist and return Events
 const concertThis = function(artist) {
     const bitApiUrl = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`;
     
+    // if an artist is provided, search for events
     if(artist) {
         request(bitApiUrl, function(error, response, body) { 
             if(error) {
-            return console.log('error:', error); // print the error if one occurs.
+                return console.log(`ERROR: Failed to call API endpoint at '${error.host}:${error.port}', resultCode: ${error.code}`);
             }
+            // Parse the body into a JSON object
             const results = JSON.parse(body);
             console.log('====================');
+            // For each event returned display the venue, city, and region|country (when outside the United States)
             results.forEach(event => {
                 if(event.venue.country === "United States") {
                     console.log(`${event.venue.name} (${event.venue.city}, ${event.venue.region})`);
@@ -26,44 +32,59 @@ const concertThis = function(artist) {
             console.log('====================');
         });
     } else {
-        console.log('Please provide an artist.');
+        console.log(`ERROR: No artist specified. Please provide an artist name to search for events.`);
     }
 }
 
-// spotify-this-song
-// TO-DO: add condition is no trackName is provided
+// Query Spotify API by Track and return details
 const spotifyThisSong = function(trackName) {
-    if(trackName) {
-    spotify.search({ type: 'track', query: trackName, limit: 5 }, function(error, data) {
-        if (error) {
-          return console.log('Error: ' + error);
-        }
-        const tracks = data.tracks.items;
-        tracks.forEach(track => {
-            console.log('====================');
-            console.log('Album:', track.album.name);
-            console.log('Track:', track.name);
-            
-            // foreach artist
-            track.artists.forEach(artist => {
-                console.log('Artist:', artist.name);
-            });
+    
+    const searchSpotify = function(options) {
+        spotify.search(options, function(error, data) {
+            if (error) {
+                return console.log(`ERROR: Failed to call API endpoint at '${error.error.host}:${error.error.port}', resultCode: ${error.error.code}`);
+            }
+            const tracks = data.tracks.items;
+            tracks.forEach(track => {
+                console.log('====================');
+                console.log('Album:', track.album.name);
+                console.log('Track:', track.name);
+                
+                track.artists.forEach(artist => {
+                    console.log('Artist:', artist.name);
+                });
 
-            console.log('Preview:', track.preview_url);
-            console.log('====================');
+                console.log('Preview:', track.preview_url);
+                console.log('====================');
+            });
         });
-      });
+    }
+
+    // if trackName was provided query for upto 5 matches else return a default. 
+    if(trackName) {
+        let options = {
+            type: "track",
+            query: trackName,
+            limit: 5
+        }
+        searchSpotify(options);
     } else {
-        console.log("What's My Age Again by blink-182");
+        let options = {
+            type: "track",
+            query: "What's My Age Again",
+            limit: 1
+        }
+        console.log(`You didn't specify a song title, so check out this one...`);
+        searchSpotify(options);
     }
 }
 
-// movie-this
+// Query the OMDB API for movie title and display the result
 const movieThis = function(movieTitle) {
     const omdbApiUrl = encodeURI(`https://www.omdbapi.com/?apikey=${keys.omdb}&t=${movieTitle}`);
     request(omdbApiUrl, function(error, response, body) {
         if(error) {
-            return console.log(`ERROR: Failed to call API endpoint at '${error.host+error.port}, eCode: ${error.code}'`);
+            return console.log(`ERROR: Failed to call API endpoint at '${error.host}:${error.port}', resultCode: ${error.code}`);
         } else {
             result = JSON.parse(body);
             console.log('====================');
@@ -80,19 +101,20 @@ const movieThis = function(movieTitle) {
     });
 }
 
-// do-what-it-says
+// Execute the query from a file
 const doWhatItSays = function(filePath) {
     fs.readFile(filePath, 'utf8', function(error, data) {
         if(error) {
             return console.log('error:', error);
         }
 
-        const content = data.split(',');
-        const command = content[0].trim();
-        const param = content[1].trim();
+        const content = data.split(',');    // split the data from the file into an array
+        const command = content[0].trim();  // trim the whitespace from command (el:0)
+        const param = content[1].trim();    // trim the whitespace from value (el:1)
 
+        // execute a function based on the command 
         switch (command) {
-            case 'concert-this': 
+            case 'concert-this':
                 concertThis(param);
                 break;
             case 'spotify-this-song':
@@ -102,28 +124,31 @@ const doWhatItSays = function(filePath) {
                 movieThis(param);
                 break;
             default:
-                console.log(`No operation found for '${process.argv[2]}'.`)
-                break;
+                console.log(`No operation found for '${command}'.`)
         }        
     });
 }
 
+// RUNTIME
+const command = process.argv[2];
 
-// runtime
-switch (process.argv[2]) {
+// Combine all the arguments from position 3 onward into a single string
+const value = process.argv.slice(3).join(' ');
+
+// Execute the corresponding function based on the command entered
+switch (command) {
     case 'concert-this': 
-        concertThis(process.argv.slice(3).join(' '));
+        concertThis(value);
         break;
     case 'spotify-this-song':
-        spotifyThisSong(process.argv.slice(3).join(' '));
+        spotifyThisSong(value);
         break;
     case 'movie-this':
-        movieThis(process.argv.slice(3).join(' '));
+        movieThis(value);
         break;
     case 'do-what-it-says':
         doWhatItSays('./random.txt');
         break;
     default:
-        console.log(`No operation found for '${process.argv[2]}'.`)
-        break;
+        console.log(`No operation found for '${command}'.`)
 }
