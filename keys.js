c9ff5e00b4c34c8268ede649c3ceda20963f5301
Michael High <mhigh85@gmail.com>
2018-12-07 15:59:16 -0500

    cleaned up keys.js file

--- keys.js
+++ keys.js
@@ -1,13 +1,8 @@
 console.log('this is loaded');
 
-// exports.spotify = {
-//     id: process.env.SPOTIFY_ID,
-//     secret: process.env.SPOTIFY_SECRET
-// };
-
-
 exports.spotify = {
-    id: process.env.SPOTIFY_ID, 
+    id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
-}
+};
+
 exports.omdb = process.env.OMDB_KEY;
